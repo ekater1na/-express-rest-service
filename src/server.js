aa66d42695063e02ac860e79e441236919e1068f
ekater1na <ekaterina_l@tut.by>
2020-04-12 02:19:11 +0300

    feat: add http-status-codes

--- src/server.js
+++ src/server.js
@@ -4,3 +4,15 @@ const app = require('./app');
 app.listen(PORT, () =>
   console.log(`App is running on http://localhost:${PORT}`)
 );
+
+process.on('uncaughtException', error => {
+  console.error(`captured error: ${error.message}`);
+  // fs.writeFileSync...
+  // process.exit(1);
+});
+
+process.on('unhandledRejection', reason => {
+  console.error(`Unhandled rejection detected: ${reason.message}`);
+});
+
+// Promise.reject(Error('Oops!'));
