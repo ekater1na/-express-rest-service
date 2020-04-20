9b636d120568dbeeab4ca5235172b9d2ee17d96f
ekater1na <ekaterina_l@tut.by>
2020-04-20 15:36:19 +0300

    feat: chanhe logger format

--- src/helpers/logger.js
+++ src/helpers/logger.js
@@ -1,17 +1,16 @@
 const { createLogger, format, transports } = require('winston');
 
 const logger = createLogger({
+  format: format.combine(format.timestamp(), format.json()),
   transports: [
     new transports.Console(),
     new transports.File({
       filename: 'logs/error.log',
-      level: 'error',
-      format: format.combine(format.timestamp(), format.json())
+      level: 'error'
     }),
     new transports.File({
       filename: 'logs/info.log',
-      level: 'info',
-      format: format.combine(format.timestamp(), format.json())
+      level: 'info'
     })
   ]
 });
